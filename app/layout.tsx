6152696f03fdf68a58ac350e9f2de76df6127036
zainali453 <zainyounas101@gmail.com>
2025-01-06 02:39:36 +0500

    adjusted eslinterrors

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "./components/theme-provider";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import Head from "next/head";
 
 export default function Home() {
   return (
